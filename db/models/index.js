e3363a74852c875f0d0e92cf4258f4da8b04b48c
cieko <rahulsaxena2021@hotmail.com>
2023-09-24 20:35:07 +0530

    sequelize init
    sequelize is the orm for postgresql, also supports other databases.

---
+++ db/models/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+const modelExport = {};
+modelExport.forEach(file => {
+    const model = file(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
